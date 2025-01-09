4a3a0755e4ccbd3565c6054fc5f440d0b036d30d
Anditoxtrading <157759124+Anditoxtrading@users.noreply.github.com>
2025-01-09 19:35:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_key = 'vfHvyWfzkNDUJIz3Sl'
-api_secret = 'nMPFdiktNKd5WF6oqRjpkrcM0PM9Umbu2zKz'
+api_key = ''
+api_secret = ''
 
 # Bot de telegram
-token_telegram='6858227834:AAFCrkSvOocikihP8HWsfBhcmUgGji-fRic'
-chat_id="1741049813"
+token_telegram=':'
+chat_id=""
